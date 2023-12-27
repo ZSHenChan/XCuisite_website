d46f0dc2e458eac524a30fa67ba9e6e3c1d171b7
ZShen <zishenchan@gmail.com>
2023-12-27 23:07:54 +0800

    fix grid-gallery, add new section

--- js/gridAnimation.js
+++ js/gridAnimation.js
@@ -11,11 +11,38 @@ const observer = new IntersectionObserver((entries) => {
     })
 })
 
+const captionObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('fadeInUp');
+        } 
+        
+    })
+})
+const textLineObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+        
+    })
+})
+
 const grid1 = document.querySelector('.grid-item-2');
-const grid2 = document.querySelector('.grid-item-3');
+const grid2 = document.querySelector('.grid-item-7');
 const grid3 = document.querySelector('.grid-item-4');
 const grid4 = document.querySelector('.grid-item-5');
+const caption = document.getElementById("grid-gallery-caption");
 observer.observe(grid1);
 observer.observe(grid2);
 observer.observe(grid3);
 observer.observe(grid4);
+captionObserver.observe(caption);
+
+//post intro
+const textLines = document.querySelectorAll('.text-lines');
+textLines.forEach((textLine) =>{
+    textLineObserver.observe(textLine);
+})
