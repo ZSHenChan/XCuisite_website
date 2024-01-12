3a3c868ff721b0bfe26695afb2b542e46176539c
Zhen <zishenchan@gmail.com>
2024-01-12 10:22:10 +0800

    add intro cta button
    need to fix the timing to dissapear

--- js/gridAnimation.js
+++ js/gridAnimation.js
@@ -1,41 +1,26 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-            entry.target.classList.remove('hidden');
-        }
-        
-    })
-})
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      entry.target.classList.remove("hidden");
+    }
+  });
+});
 
 const captionObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('fadeInUp');
-        } 
-        
-    })
-})
-const textLineObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        } else{
-            entry.target.classList.remove('show');
-        }
-        
-    })
-})
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("fadeInUp");
+    }
+  });
+});
 
-const grid1 = document.querySelector('.grid-item-2');
-const grid2 = document.querySelector('.grid-item-7');
-const grid3 = document.querySelector('.grid-item-4');
-const grid4 = document.querySelector('.grid-item-5');
+const grid1 = document.querySelector(".grid-item-2");
+const grid2 = document.querySelector(".grid-item-7");
+const grid3 = document.querySelector(".grid-item-4");
+const grid4 = document.querySelector(".grid-item-5");
 const caption = document.getElementById("grid-gallery-caption");
 observer.observe(grid1);
 observer.observe(grid2);
 observer.observe(grid3);
 observer.observe(grid4);
-captionObserver.observe(caption);
-
-
--- js/video.js
+++ js/video.js
@@ -1,41 +1,67 @@
-const intro = document.querySelector('.intro');
-const video = document.getElementById('video-desktop');
-const videoMobile = document.getElementById('video-mobile');
-videoMobile.playbackRate=2;
-const text = intro.querySelector('h1');
+const introSectionFunc = function () {
+  const intro = document.querySelector(".intro");
+  const video = document.getElementById("video-desktop");
+  const videoMobile = document.getElementById("video-mobile");
+  const text = intro.querySelector("h1");
+  const introCtaBtn = document.querySelector(".intro-cta-btn");
+  const postIntro = document.querySelector(".post-intro");
+  // const controller = new ScrollMagic.Controller();
 
-const controller = new ScrollMagic.Controller();
+  window.addEventListener("scroll", function () {
+    let elementHeight = video.offsetHeight;
+    let scrollTop = this.window.scrollY;
+    offset = Math.min(elementHeight, scrollTop);
+    opacity = 1 - offset / 300;
+    video.style.opacity = opacity;
+  });
 
-
-window.addEventListener("scroll", function(){
+  window.addEventListener("scroll", function () {
     let elementHeight = videoMobile.offsetHeight;
     let scrollTop = this.window.scrollY;
-    offset = Math.min(elementHeight,scrollTop);
-    opacity = 1-offset/300;
+    offset = Math.min(elementHeight, scrollTop);
+    opacity = 1 - offset / 300;
     videoMobile.style.opacity = opacity;
-})
+  });
 
+  const introCtaBtnAnimation = function (entries) {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      introCtaBtn.classList.remove("expand-btn");
+      introCtaBtn.classList.add("contract-btn");
+    } else {
+      introCtaBtn.classList.add("expand-btn");
+      introCtaBtn.classList.remove("contract-btn");
+    }
+  };
 
-//Scenes
-// if (window.getComputedStyle(video).getPropertyValue("display")=="block"){
-//     let scene = new ScrollMagic.Scene({
-//         duration: 4800,
-//         triggerElemnt: intro,
-//         triggerHook: 0.5
-//     }).addTo(controller).setPin(intro);
-    
-//     //Animation
-//     let accelamount=0.1;
-//     let delay=0;
-//     let scrollpos = 0;
-    
-//     scene.on('update', e => {
-//         scrollpos = e.scrollPos/1000;
-//     })
-    
-//     setInterval(() => {
-//         delay+= (scrollpos - delay) *accelamount;
-//         video.currentTime = scrollpos;
-//     }, 33.3)
-// }
+  const introCtaObserver = new IntersectionObserver(introCtaBtnAnimation, {
+    root: null,
+    threshold: 0.1,
+  });
+  introCtaObserver.observe(postIntro);
 
+  //Scenes
+  // if (window.getComputedStyle(video).getPropertyValue("display")=="block"){
+  //     let scene = new ScrollMagic.Scene({
+  //         duration: 4800,
+  //         triggerElemnt: intro,
+  //         triggerHook: 0.5
+  //     }).addTo(controller).setPin(intro);
+
+  //     //Animation
+  //     let accelamount=0.1;
+  //     let delay=0;
+  //     let scrollpos = 0;
+
+  //     scene.on('update', e => {
+  //         scrollpos = e.scrollPos/1000;
+  //     })
+
+  //     setInterval(() => {
+  //         delay+= (scrollpos - delay) *accelamount;
+  //         video.currentTime = scrollpos;
+  //     }, 33.3)
+  // }
+};
+
+introSectionFunc();
